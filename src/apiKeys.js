b5e6cddc761ffbd1fe91896c9729e8cba21828e4
suyashpanwar <suyashp124@gmail.com>
2024-04-18 09:29:09 +0530

    final commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{54e1b70c97eb127e2cfd6b55c10b0ff9}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
